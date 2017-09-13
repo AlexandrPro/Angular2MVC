347f63a618d59d6271fe2b68fb2780254f64e8dd
Prokofiev Alexander <Alexandr.Prokofiev.A@gmail.com>
2017-09-13 14:33:13 +0300

    Create by https://www.codeproject.com/Articles/1181888/Angular-in-ASP-NET-MVC-Web-API-Part

---
+++ Angular2MVC/Controllers/UserAPIController.cs
@@ -0,0 +1,36 @@
+﻿using Angular2MVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Angular2MVC.Controllers
+{
+    public class UserAPIController : BaseAPIController
+    {
+        public HttpResponseMessage Get()
+        {
+            return ToJson(UserDB.Users.AsEnumerable());
+        }
+
+        public HttpResponseMessage Post([FromBody]User value)
+        {
+            UserDB.Users.Add(value);
+            return ToJson(UserDB.SaveChanges());
+        }
+
+        public HttpResponseMessage Put(int id, [FromBody]User value)
+        {
+            UserDB.Entry(value).State = EntityState.Modified;
+            return ToJson(UserDB.SaveChanges());
+        }
+        public HttpResponseMessage Delete(int id)
+        {
+            UserDB.Users.Remove(UserDB.Users.FirstOrDefault(x => x.Id == id));
+            return ToJson(UserDB.SaveChanges());
+        }
+    }
+}
---
+++ Angular2MVC/Models/UserContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Data.Entity;
+
+
+namespace Angular2MVC.Models
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base("UsersDbConnection")
+        {
+        }
+
+        public virtual DbSet<User> Users { get; set; }
+    }
+}
